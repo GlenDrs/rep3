adf67d53c19c0ba2741f0971e71a664c46eae947
Glen <67809619+GlenDu@users.noreply.github.com>
2021-07-30 00:31:19 +0200

    Update 20210729213930_create_sub_coments.rb

--- db/migrate/20210729213930_create_sub_coments.rb
+++ db/migrate/20210729213930_create_sub_coments.rb
@@ -2,7 +2,7 @@ class CreateSubComents < ActiveRecord::Migration[5.2]
   def change
     create_table :sub_coments do |t|
       t.string :content
-      t.belongs_to :comment, index: true
+      t.belongs_to :coment, index: true
       t.belongs_to :user, index: true
 
       t.timestamps
